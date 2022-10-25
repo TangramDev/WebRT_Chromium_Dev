c17da91d2c16d60b93bf184ea275fee848f9efe6
tangram <tangramteam@outlook.com>
2022-10-25 10:34:25 +0800

    add getWRTB.bat

---
+++ ChromiumSRC/getWRTB.bat
@@ -0,0 +1,2 @@
+@echo off
+git fetch origin tag 108.0.5359.%1 && git checkout -B M108.5359.%1 108.0.5359.%1&& gclient sync -D && git cherry-pick Base &&..\b1.sh&&..\bd %2&&git checkout Base&&git pull&&gclient sync -D
