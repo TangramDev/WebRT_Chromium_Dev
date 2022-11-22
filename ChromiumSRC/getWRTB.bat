58ffd21a6d0c4103df7425b504f58dc8dc7aa959
tangram <tangramteam@outlook.com>
2022-11-22 19:24:13 +0800

    modify bat files for M109

--- ChromiumSRC/getWRTB.bat
+++ ChromiumSRC/getWRTB.bat
@@ -1,2 +1,2 @@
 @echo off
-git fetch origin tag 109.0.%1 && git checkout -B M109.%1 109.0.%1&& gclient sync -D && git cherry-pick Base&&..\b1.sh&&..\bd %2&&git checkout Base&&git pull&&gclient sync -D
+git fetch origin tag 109.0.5414.%1 && git checkout -B M109.5414.%1 109.0.%1&& gclient sync -D && git cherry-pick Base&&..\b1.sh&&..\bd %2&&git checkout Base&&git pull&&gclient sync -D
--- ChromiumSRC/getbranch.bat
+++ ChromiumSRC/getbranch.bat
@@ -1,2 +1,2 @@
 @echo off
-git fetch origin tag 109.0.%1 &&git checkout -b %2 109.0.%1&& gclient sync -D
+git fetch origin tag 109.0.5414.%1 &&git checkout -b %2 109.0.5414.%1&& gclient sync -D
