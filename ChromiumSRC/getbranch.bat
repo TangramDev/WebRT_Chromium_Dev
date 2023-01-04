49e9488f866ff2225c4bd7bff6565a8dea3b8ea6
Tangram <tangramteam@outlook.com>
2023-01-05 06:08:38 +0800

    Support M110

--- ChromiumSRC/getbranch.bat
+++ ChromiumSRC/getbranch.bat
@@ -1,2 +1,2 @@
 @echo off
-git fetch origin tag 109.0.5414.%1 &&git checkout -b %2 109.0.5414.%1&& gclient sync -D
+git fetch origin tag 110.0.5481.%1 &&git checkout -b %2 110.0.5481.%1&& gclient sync -D
